23361e27b9381ac110bf092ec8e5a8a1309254b4
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2016-02-11 02:48:27 -0500

    Add browse section

--- react-app/app.js
+++ react-app/app.js
@@ -26,8 +26,23 @@ class WhoISApp extends React.Component {
     super(props)
     this.state = {
       result: undefined,
-      error: undefined
+      error: undefined,
+      saved: undefined,
+      section: 'search'
     }
+
+    this.changeSection = this.changeSection.bind(this)
+  }
+
+  componentDidMount() {
+    let self = this
+
+    // Make API call to fetch stored records
+    fetch(`http://${location.host}:666/saved`)
+  	.then(res => res.json())
+  	.then( saved => {
+      self.setState({ saved })
+    })
   }
 
   query(event) {
@@ -45,51 +60,74 @@ class WhoISApp extends React.Component {
   }
 
   saveRecord() {
+    let self = this
 
+    // Make API call to save the record
+    fetch(`http://${location.host}:666/save?website=${ this.state.website }&result=${ this.state.result }`, {
+      method: 'POST'
+    })
+    .then(res => res.json())
+    .then( json => {
+      self.setState({
+        saved: [...this.state.saved, json]
+      })
+    })
+  }
+
+  changeSection(section) {
+    this.setState({ section })
   }
 
   render() {
-    const { error, result } = this.state
+    const { error, result, section, saved } = this.state
 
     return(
       <div>
         <LeftNav open={ true }>
-          <MenuItem>Search DNS and WHOIS</MenuItem>
-          <MenuItem>Browse stored records</MenuItem>
+          <MenuItem onTouchTap={ () => this.changeSection('search') }>Search DNS and WHOIS</MenuItem>
+          <MenuItem onTouchTap={ () => this.changeSection('browse') }>Browse stored records</MenuItem>
         </LeftNav>
 
-        <div style={ style }>
-          <TextField floatingLabelText="Search website WHOIS information" fullWidth={ true } onEnterKeyDown={ this.query.bind(this) }/>
+        { section === 'search' &&
+          <div style={ style }>
+            <TextField floatingLabelText="Search website WHOIS information" fullWidth={ true } onEnterKeyDown={ this.query.bind(this) }/>
 
-        	{ error && <div style={ errorStyle }>{ error }</div> }
-        	{ result &&  <div> {
-            // Split each line break
-            result.split('\n').map((row, i) => (
-              <span key={ i }>
-                {
+          	{ error && <div style={ errorStyle }>{ error }</div> }
+          	{ result &&  <div> {
+              // Split each line break
+              result.split('\n').map((row, i) => (
+                <span key={ i }>
+                  {
 
-                  // Split each tab
-                  row.split('\t').map((col, coli) => (
-                    <span key={ coli } style={{ padding: coli === 0 ? 0 : 10 }}>{ col }</span>
-                  ))
-                }
+                    // Split each tab
+                    row.split('\t').map((col, coli) => (
+                      <span key={ coli } style={{ padding: coli === 0 ? 0 : 10 }}>{ col }</span>
+                    ))
+                  }
 
-                <br/>
-              </span>
-            ))
-          }
-          <RaisedButton
-            label="Save Record"
-            onTouchTap={this.saveRecord}
-            style={{
-              position: 'absolute',
-              top: '50px',
-              left: '-150px'
-            }}
-            secondary={true}
-          />
-          </div> }
-        </div>
+                  <br/>
+                </span>
+              ))
+            }
+            <RaisedButton
+              label="Save Record"
+              onTouchTap={this.saveRecord}
+              style={{
+                position: 'absolute',
+                top: '50px',
+                left: '-150px'
+              }}
+              secondary={true}
+            />
+            </div> }
+          </div>
+        }
+
+        { section === 'browse' && <div style={style}>
+          { saved && saved.length === 0 && <span>Nothing is stored. Please save a few records first.</span>}
+          { saved && saved.map( record => <div>{ record.website }</div> )}
+          </div>
+        }
       </div>
     )
   }
