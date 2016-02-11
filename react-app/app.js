09ab7cb2ddba9fa213c509e804a1eb1363148ef3
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2016-02-11 00:35:08 -0500

    [app.js] fixed code formatting

--- react-app/app.js
+++ react-app/app.js
@@ -2,11 +2,9 @@ import injectTapEventPlugin from 'react-tap-event-plugin'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import fetch from 'isomorphic-fetch'
-
-injectTapEventPlugin()
-
 import TextField from 'material-ui/lib/text-field'
 
+injectTapEventPlugin()
 
 const style = {
   padding: "20px 10px",
@@ -16,10 +14,6 @@ const style = {
 
 const errorStyle = {
   color: 'red'
-}
-
-const resultStyle = {
-
 }
 
 class WhoISApp extends React.Component {
@@ -33,28 +27,28 @@ class WhoISApp extends React.Component {
   }
 
   query(event) {
-let self = this
+    let self = this
+
+    // Make API call to see dig and whois data of the website
     fetch(`http://${location.host}:666?website=${event.target.value}`)
-	.then(res => {
-	  return res.json()
-	})
-	.then( json => {
-	  self.setState({
-            error: json.error,
-            result: json.result
-          })
-        })
+  	.then(res => res.json())
+  	.then( json => {
+     self.setState({
+        error: json.error,
+        result: json.result
+      })
+    })
   }
 
   render() {
     const { error, result } = this.state
-    
+
     return(
       <div style={ style }>
         <TextField floatingLabelText="Search website WHOIS information" fullWidth={ true } onEnterKeyDown={ this.query.bind(this) }/>
 
-	{ error && <div style={ errorStyle }>{ error }</div> }
-	{ result &&  <div style={ resultStyle }>{ result.split('\n').map((row, i) => <span key={ i }>{ row.split('\t').map((col, coli) => <span key={ coli } style={{ padding: coli === 0 ? 0 : 10 }}>{ col }</span> ) }<br/></span>) }</div> }
+      	{ error && <div style={ errorStyle }>{ error }</div> }
+      	{ result &&  <div>{ result.split('\n').map((row, i) => <span key={ i }>{ row.split('\t').map((col, coli) => <span key={ coli } style={{ padding: coli === 0 ? 0 : 10 }}>{ col }</span> ) }<br/></span>) }</div> }
       </div>
     )
   }
