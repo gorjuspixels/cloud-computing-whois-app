8a2371c6d460417e86a87424097a36e24717d2ac
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2016-02-11 00:32:38 -0500

    Add mongo, a route to save, and a route to see saved records

--- app.js
+++ app.js
@@ -1,22 +1,66 @@
 import express from 'express'
 import { exec } from 'child_process'
+import { WhoIsRecord } from './db'
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
+// GET /whois - Route for getting whois and dig command output
+app.get('/whois', (req, res) => {
+  const { website } = req.query
+  if (!website) return res.json({ error: 'WHO IS ________' })
 
-  exec(`dig ${req.query.website}`, (err, stdout, stderr) => {
-    if (err) { return res.json({error: err}) }
+  // Execute `dig` command to get domain's DNS results
+  exec(`dig ${website}`, (digErr, digStdOut) => {
 
-    exec(`whois ${req.query.website}`, (err1, stdout1, stderr1) => {
-      if (err) { return res.json({error: err}) }
-      res.json({result: stdout + '\n\n\n' + stdout1})
+    // If we got an error, return it to the client
+    if (digErr) { return res.json({error: digErr}) }
+
+    // Check whois information of the domain if it is available
+    exec(`whois ${website}`, (whoisErr, whoisStdOut) => {
+      if (whoisErr) { return res.json({error: whoisErr}) }
+
+      // Return dig and whois information back to the client
+      res.json({result: digStdOut + '\n\n\n' + whoisStdOut})
     })
-
   })
-
 })
 
+
+// POST /save - Route to save website information
+app.post('/save', (req, res) => {
+  const { website, result } = req.query
+  if (!website) return res.json({ error: 'WHO IS ________' })
+
+  // Create new record
+  const record = new WhoIsRecord({
+    record: result,
+    website
+  })
+
+  // Save the record in the db
+  record.save((err, record) => {
+
+    // Check for error
+    if (err) return res.json({error: err})
+
+    // Return saved record to the client
+    res.json(record)
+  })
+})
+
+// GET /saved - Retrieve all saved records
+app.get('/saved', (req, res) => {
+  WhoIsRecord.find({}, (err, records) => {
+    if (err) {
+      return res.json({error: err})
+    }
+
+    res.json(records)
+  })
+})
+
+
+// Start the server
 app.listen(PORT, function () {
   console.log(`Whois app listening on port ${PORT}!`);
 })
---
+++ db.js
@@ -0,0 +1,14 @@
+import mongoose from mongoose
+mongoose.connect('mongodb://localhost/whois')
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'Connection error:'))
+db.once('open', function() {
+  // we're connected!
+  console.log('Established connection to the database')
+})
+
+export const WhoIsRecord = mongoose.model('WhoIsRecord', {
+  website: String,
+  record: String
+})
